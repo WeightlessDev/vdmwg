fed4706b08b7daac6702c3c724afafa7c465bb4c
Denis Garcia <dev@weightlessgames.com>
2018-07-11 08:54:07 -0600

    Testing more info in hotspots.

--- data.js
+++ data.js
@@ -42,7 +42,7 @@ var APP_DATA = {
     },
     {
       "id": "1-habitacion-master",
-      "name": "Habitacion Master",
+      "name": "Habitacion Master \n 14.5 Metros cuadrados. \n Baño privado.",
       "levels": [
         {
           "tileSize": 256,
